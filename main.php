8e05896515eb32415fb05cfe82ac2a5e7f59b2f5
mervburger <mervburger@gmail.com>
2019-04-27 13:42:42 -0400

    Add media upload status checking (for #1)
    - Provided by an update to TwitterOAuth.
    - Added debug information output with the upload process

--- main.php
+++ main.php
@@ -126,18 +126,51 @@ function postTweet($post, $filename) {
 	$connection->setTimeouts(30, 120);
 
 	// Upload file and generate the media_id
+	echo "Uploading to Twitter...\n";
+	$timer = time();
 	$picture = $connection->upload('media/upload', ['media' => getcwd() . '/' . $filename, 'media_type' => mime_content_type(getcwd() . '/' . $filename)], true);
+	echo "Finished upload. (" . time() - $timer . " seconds)\n";
+
+	// We never need the actual image file after this, just unlink it now.
+	unlink($filename);
 
 	// We need to wait while twitter potentially need to processes our upload
 	// From: https://github.com/abraham/twitteroauth/issues/554
 	// This is a lazy solution until proper STATUS checking is in TwitterOAuth
-	echo "Waiting for twitter processing...\n";
-	sleep(30);
+	//echo "Waiting for twitter processing...\n";
+	//sleep(30);
 
-	if ( $connection->getLastHttpCode() != 201) {
+	/*if ( $connection->getLastHttpCode() != 201) {
 		unlink($filename);
 		echo "File not uploaded successfully: " . $connection->getLastHttpCode() . "\n";
 		die;
+	}*/
+	//print_r($picture);
+	//print_r($connection->getLastHttpCode()); echo "\n";
+	
+	// Status checking only needs to be done on videos and gifs if Twitter says it needs to be done
+	// with the processing_info property after a FINALIZE command
+	if ( $connection->getLastHttpCode() != 201 ) {
+		if ( proptery_exists($picture->processing_info) ) {
+			$limit = 0;
+			do {
+				echo "Waiting for twitter processing... \n";
+				$upStatus = $connection->mediaStatus($picture->media_id_string);
+				sleep(5);
+				$limit++;
+				if ( $limit > 10 ) {
+					echo "Limit exceeded! Tweet will likely fail! Debug: \n";
+					print_r($picture);
+					print_r($upStatus);
+				}
+			} while ( $upStatus->processing_info->state !== 'succeeded' && $limit <= 10 );
+		} else {
+			echo "File upload unsuccessful! \n";
+			print_r($picture);
+			echo "Status Code: ";
+			print_r($connection->getLastHttpCode());
+			echo "\n";
+		}
 	}
 
 	// Generate status text. This prefers crediting the artist over character names
@@ -163,10 +196,10 @@ function postTweet($post, $filename) {
 
 	if ( $connection->getLastHttpCode() == 200 ) {
 		echo "Completed successfully!\n";
-		unlink($filename);
+		//unlink($filename);
 		return true;
 	} else {
-		unlink($filename);
+		//unlink($filename);
 		echo "Tweet not sent correctly: " . $connection->getLastHttpCode() . "\n";
 		die;
 	}
