f867467e880f0cd12389cabe925252ec582f4e64
mervburger <mervburger@gmail.com>
2019-12-26 13:31:12 -0500

    Update auth to have danbooru login vars

--- auth.sample.php
+++ auth.sample.php
@@ -4,4 +4,7 @@ $consumer_secret = '[Your Twitter API Consumer Secret]';
 
 $access_token = '[Your Twitter Account Access Token]';
 $access_secret = '[Your Twitter Account Access Token Secret]';
+
+$booru_login = '[Your Danbooru Username]';
+$booru_key = '[Your Danbooru API Key]';
 ?>
