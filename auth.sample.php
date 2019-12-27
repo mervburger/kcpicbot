2d90e1d39fed6fcd4ec3fbeda9c2eac43a97a34d
mervburger <mervburger@gmail.com>
2019-12-26 21:31:39 -0500

    Add comments to  sample auth file

--- auth.sample.php
+++ auth.sample.php
@@ -1,10 +1,12 @@
 <?php
+//Twitter App and User API keys
 $consumer_key = '[Your Twitter API Consumer Key]';
 $consumer_secret = '[Your Twitter API Consumer Secret]';
 
 $access_token = '[Your Twitter Account Access Token]';
 $access_secret = '[Your Twitter Account Access Token Secret]';
 
+//Danbooru API access login/key. Blank these variables for anonymous access
 $booru_login = '[Your Danbooru Username]';
 $booru_key = '[Your Danbooru API Key]';
 ?>
